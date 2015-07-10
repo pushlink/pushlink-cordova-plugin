a0b6d60e49b80bb92538c0b52d252671ecb28882
Rodolfo Carvalho <rodolfo@infweb.net>
2015-07-10 07:26:04 -0400

    Updated files to fix issues with ESLint

--- tests/tests.js
+++ tests/tests.js
@@ -1,30 +1,34 @@
 /* eslint-env jasmine */
+/* globals PushLink */
+'use strict';
 
 var checkCallsPushLink = function(action, successCallback, errorCallback, arg) {
-  expect(window.cordova.exec).toHaveBeenCalledWith(successCallback, errorCallback, 'com.pushlink.cordova.PushLinkPlugin', action, [arg]);
-}
+  expect(window.cordova.exec).toHaveBeenCalledWith(
+    successCallback, errorCallback, 'com.pushlink.cordova.PushLinkPlugin',
+    action, [arg]);
+};
 
 exports.defineAutoTests = function() {
-  describe("PushLink object (window.PushLink)", function() {
-    it("should exists", function() {
+  describe('PushLink object (window.PushLink)', function() {
+    it('should exists', function() {
       expect(window.PushLink).toBeDefined();
-      expect(typeof window.PushLink).toBe("object");
+      expect(typeof window.PushLink).toBe('object');
     });
 
-    it("should return the right version", function(done) {
+    it('should return the right version', function(done) {
       PushLink.getVersion(function(data) {
         expect(data.version).toBe('5.3.0');
         done();
       },
       function() {
         console.error(arguments);
-        jasmine.fail("Test failed");
+        jasmine.fail('Test failed');
         done();
       });
     });
   });
 
-  describe("PushLink service calls", function() {
+  describe('PushLink service calls', function() {
     var realCordova = null;
     beforeEach(function() {
       realCordova = window.cordova;
@@ -35,14 +39,17 @@ exports.defineAutoTests = function() {
       window.cordova = realCordova;
     });
 
-    it("should start the service", function() {
+    it('should start the service', function() {
       expect(PushLink.start).toBeDefined();
-      expect(typeof PushLink.start).toBe("function");
+      expect(typeof PushLink.start).toBe('function');
 
-      PushLink.start("com.example", 'api-key', 'device-id', null, null);
+      PushLink.start('com.example', 'api-key', 'device-id', null, null);
 
-      checkCallsPushLink('start', null, null, {packageName: "com.example", apiKey: 'api-key', deviceId: 'device-id'});
+      checkCallsPushLink('start', null, null, {
+        packageName: 'com.example',
+        apiKey: 'api-key',
+        deviceId: 'device-id'
+      });
     });
   });
-
 };
